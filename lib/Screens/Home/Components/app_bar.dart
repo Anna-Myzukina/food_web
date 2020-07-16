55597a7ebd8df80d7f597b997a5ab560364392f9
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-07-17 00:30:17 +0300

    Add navbar

--- lib/Screens/Home/Components/app_bar.dart
+++ lib/Screens/Home/Components/app_bar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:food_web/Components/default_button.dart';
+import 'menu_item.dart';
 
 class CustomAppBar extends StatelessWidget {
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(20),
@@ -17,8 +19,44 @@ class CustomAppBar extends StatelessWidget {
         ],
       ),
       child: Row(
-        
-      )
+        children: <Widget>[
+          Image.asset(
+            "assets/images/logo.png",
+            height: 25,
+            alignment: Alignment.topCenter,
+          ),
+          SizedBox(width: 5),
+          Text("FoodWeb".toUpperCase(),
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+          Spacer(),
+          MenuItem(
+            title: "Home",
+            press: () {},
+          ),
+          MenuItem(
+            title: "about",
+            press: () {},
+          ),
+          MenuItem(
+            title: "Pricing",
+            press: () {},
+          ),
+          MenuItem(
+            title: "Contact",
+            press: () {},
+          ),
+          MenuItem(
+            title: "Login",
+            press: () {},
+          ),
+          DefaultButton(
+            text: "Get Started",
+            press: () {},
+          ),
+        ],
+      ),
     );
   }
 }
+
+
